8ddfe466d22e76a131d2cf4c1bbfbf3275995ade
Jenny Chen <chen1900_seu@163.com>
2018-03-20 22:37:32 +0800

    Binary search for the LeetCode441.

--- src/Algorithm/Arranging_Coins.java
+++ src/Algorithm/Arranging_Coins.java
@@ -1,6 +1,7 @@
 package Algorithm;
 
 /**
+ * LeetCode 441
  You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
  Given n, find the total number of full staircase rows that can be formed.
  n is a non-negative integer and fits within the range of a 32-bit signed integer.
@@ -35,7 +36,7 @@ public class Arranging_Coins {
             else
                 h = mid-1;
         }
-        return h;
+        return h;//边界判断
     }
 
     public static int arrangecoins3(int n){
---
+++ src/Algorithm/Single_Element_in_Sorted_Array.java
@@ -0,0 +1,36 @@
+package Algorithm;
+
+/**
+ 540. Single Element in a Sorted Array
+ Given a sorted array consisting of only integers where every element appears twice except for one element which appears once.
+ Find this single element that appears only once.
+ Example 1:
+ Input: [1,1,2,3,3,4,4,8,8]
+ Output: 2
+ Example 2:
+ Input: [3,3,7,7,10,11,11]
+ Output: 10
+ Note: Your solution should run in O(log n) time and O(1) space.
+ */
+//如果mid处在偶数位置！则有相同数字在哪边则那边就是有单一数字的半边。
+public class Single_Element_in_Sorted_Array {
+    public static int singleNonDuplicate(int[] nums) {
+        int l=0,h=nums.length-1;
+        while (l<h){//退出的条件永远都是l=h
+            int mid = l+(h-l)/2;
+            if(mid%2 == 1)
+                mid = mid-1;
+//            System.out.println(mid);
+            if(nums[mid]==nums[mid+1])
+                l=mid+2;
+            else
+                h=mid;//这里不需要提供elseif去返回nums[mid]
+        }
+        return nums[l];
+    }
+    public static void main(String[] args) {
+        int[] nums ={1,1,2};
+        int result=singleNonDuplicate(nums);
+        System.out.println(result);
+    }
+}
