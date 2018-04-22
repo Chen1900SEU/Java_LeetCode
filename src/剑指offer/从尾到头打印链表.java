e00f2000325e57f1183db0ad2aa0129a80ffa54a
Jenny Chen <chen1900_seu@163.com>
2018-04-22 19:30:40 +0800

    2018.04.22

--- src/剑指offer/从尾到头打印链表.java
+++ src/剑指offer/从尾到头打印链表.java
@@ -19,65 +19,65 @@ class ListNode {
     }
 }
 
-class ListNodeOperation {
-
-    //创建链表
-    public ListNode createListNode(){
-        System.out.println("请输入节点值以-1结束");
-        Scanner scan = new Scanner(System.in);
-        ListNode pHead= new ListNode();
-        int temp;
-        if((temp=scan.nextInt())==-1){//头结点
-            return null;
-        }else{
-            pHead.val=temp;
-        }
-
-        ListNode p =pHead;
-        while((temp=scan.nextInt())!=-1){
-            ListNode q = new ListNode(temp);
-            p.next=q;
-            p=p.next;
-        }
-        p.next=null;
-        return pHead;
-    }
-
-    //打印链表
-    public void display(ListNode pHead){
-        while(pHead!=null){
-            System.out.print(pHead.val+"\t");
-            pHead=pHead.next;
-        }
-        System.out.println();
-    }
-    //在某位置增加一个节点，pos=0表示在头结点前插入
-    public ListNode addNode(ListNode pHead,int pos,int value){
-        ListNode q = new ListNode(value);
-        if(pos<0){
-            System.out.println("插入失败");
-            return pHead;
-        }
-        if(pos==0){//在头结点前插入
-            q.next=pHead;
-            return q;
-        }
-        ListNode p = pHead;
-        while((--pos)>0&&p!=null){
-            p=p.next;
-        }
-        if(p==null){
-            System.out.println("插入失败！");
-            return pHead;
-        }
-        q.next=p.next;
-        p.next=q;
-        return pHead;
-
-    }
-
-
-}
+//class ListNodeOperation {
+//
+//    //创建链表
+//    public ListNode createListNode(){
+//        System.out.println("请输入节点值以-1结束");
+//        Scanner scan = new Scanner(System.in);
+//        ListNode pHead= new ListNode();
+//        int temp;
+//        if((temp=scan.nextInt())==-1){//头结点
+//            return null;
+//        }else{
+//            pHead.val=temp;
+//        }
+//
+//        ListNode p =pHead;
+//        while((temp=scan.nextInt())!=-1){
+//            ListNode q = new ListNode(temp);
+//            p.next=q;
+//            p=p.next;
+//        }
+//        p.next=null;
+//        return pHead;
+//    }
+//
+//    //打印链表
+//    public void display(ListNode pHead){
+//        while(pHead!=null){
+//            System.out.print(pHead.val+"\t");
+//            pHead=pHead.next;
+//        }
+//        System.out.println();
+//    }
+//    //在某位置增加一个节点，pos=0表示在头结点前插入
+//    public ListNode addNode(ListNode pHead,int pos,int value){
+//        ListNode q = new ListNode(value);
+//        if(pos<0){
+//            System.out.println("插入失败");
+//            return pHead;
+//        }
+//        if(pos==0){//在头结点前插入
+//            q.next=pHead;
+//            return q;
+//        }
+//        ListNode p = pHead;
+//        while((--pos)>0&&p!=null){
+//            p=p.next;
+//        }
+//        if(p==null){
+//            System.out.println("插入失败！");
+//            return pHead;
+//        }
+//        q.next=p.next;
+//        p.next=q;
+//        return pHead;
+//
+//    }
+//
+//
+//}
 
 public class 从尾到头打印链表 {
 
